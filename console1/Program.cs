7f217564e96d04bdb0bbc66f7921384662088dc1
Hans Wurst <nobsdb@gmx.de>
2012-04-10 17:14:18 +0200

    initial load

---
+++ console1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Das ist ein Test");
+            Console.ReadLine();
+        }
+    }
+}
