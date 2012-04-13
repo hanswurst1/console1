cd3b36c69a9728e538acebe758e958845fa50e40
Hans Wurst <nobsdb@gmx.de>
2012-04-13 11:17:51 +0200

    erwin integration

--- console1/Program.cs
+++ console1/Program.cs
@@ -15,7 +15,35 @@ namespace console1
             Console.WriteLine("Unbhkb hkd noch ein Test!");
             int h = 5 + 1;
             Console.WriteLine(h);
+
+            erwin er = new erwin();
+            er.A = 5;
+            er.B = 10;
+            Console.WriteLine(er.bere().ToString());
+
             Console.ReadLine();
+        }
+
+        struct erwin
+        {
+            private int a;
+            public int A
+            {
+              get { return a; }
+              set { a = value; }
+            }
+
+            private int b;
+            public int B
+            {
+              get { return b; }
+              set { b = value; }
+            }
+
+            public int bere()
+            {
+                return this.A + this.B;
+            }
         }
     }
 }
